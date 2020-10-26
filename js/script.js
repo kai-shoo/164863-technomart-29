2870ea18b8b4f5a6c6cf2e10108303ad8aa035a6
Kai Shoo <qsteamk@gmail.com>
2020-10-26 03:36:47 +0300

    made final polish

--- js/script.js
+++ js/script.js
@@ -32,6 +32,9 @@ for (var i = 0; i < buttonBookmark.length; i++) {
     self.addEventListener("click", function(evt) {
         evt.preventDefault();
         var int = parseInt(bookmark.textContent.replace("Закладки: ", ""));
+        if (!int) {
+            bookmark.classList.add("purchase-item_notempty");
+        }
         bookmark.textContent = bookmark.textContent.replace(int, ++int);
     });
 }
@@ -43,6 +46,10 @@ buttonClose.forEach(function(self) {
         evt.preventDefault();
         self.closest(".modal").style.display = "none";
     });
+    self.addEventListener("focusout", function(evt) {
+        evt.preventDefault();
+        self.closest(".modal").focus();
+    })
 });
 
     
@@ -72,5 +79,60 @@ contactsButton.addEventListener("click", function(evt) {
 
 
 
-// double range slider 
+// slider
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider-item");
+  var dots = document.getElementsByClassName("slider__status");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" slider_active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " slider_active";
+} 
+
+
+
+
+// vertical tabs
+
+tabcontent = document.getElementsByClassName("services-content-item");
+    for (i = 1; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+var i, tabcontent, tablinks;
+tablinks = document.getElementsByClassName("toggle__tab");
+tabcontent = document.getElementsByClassName("services-content-item");
+
+function openTab(evt, tabName) {
+    // Declare all variables
+
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" enabled", "");
+    }
+  
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " enabled";
+  } 
